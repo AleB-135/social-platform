ba9da87d3eb2d8f3e6b825f718c43159a6ec58ee
AleB-135 <alessiobrusca1999@outlook.com>
2025-03-31 18:54:29 +0200

    feat: Eseguita seconda metà milestone

--- query.sql
+++ query.sql
@@ -17,16 +17,25 @@ SELECT `title`, `posts`.`date`, `post_id`,`likes`.`user_id`
 FROM `posts`
 LEFT JOIN `likes`
 ON `posts`.`id` = `likes`.`post_id`
-WHERE `likes`.`post_id` IS NULL
+WHERE `likes`.`post_id` IS NULL;
 
 -- Conta il numero di like per ogni post (165 nel caso di query con SELECT + JOIN oppure 175 nel caso di query con sola SELECT)
 
-SELECT COUNT(`post_id`)
+SELECT `post_id`, COUNT(*) AS `numero_likes`
 FROM `likes`
-GROUP BY `post_id`
+GROUP BY `post_id`;
 
 -- Ordina gli utenti per il numero di media caricati (25) 
 
+SELECT `user_id`, COUNT(*) AS `media_caricati`
+FROM `medias`
+GROUP BY `user_id`
+ORDER BY `media_caricati` ASC;
 
 
 -- Ordina gli utenti per totale di likes ricevuti nei loro posts (25) 
+
+SELECT `user_id`, COUNT(*) AS `likes_ricevuti`
+FROM `likes`
+GROUP BY `user_id`
+ORDER BY `likes_ricevuti` ASC;
